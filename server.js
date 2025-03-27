b8d1e1b242f5a8009b3f6747c1e6227d0940232b
MD MUFID ALAM <mdmufid2004@gmail.com>
2025-03-27 20:22:36 +0530

    Backend update for PeerJS fix

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ const app = express();
 app.use(cors());
 
 const server = http.createServer(app);
-
-// Create PeerJS server
 const peerServer = ExpressPeerServer(server, {
   debug: true,
-  path: "/peerjs",
+  path: "/myapp",
 });
 
 app.use("/peerjs", peerServer);
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(5000, () => {
+  console.log("PeerJS server running on port 5000");
 });
